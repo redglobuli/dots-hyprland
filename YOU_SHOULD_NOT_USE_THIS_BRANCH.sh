04d5475b243f84ed53f530cad88b414a0e64cc39
end-4 <97237370+end-4@users.noreply.github.com>
2023-12-01 17:40:49 +0700

    a

--- YOU_SHOULD_NOT_USE_THIS_BRANCH.sh
+++ YOU_SHOULD_NOT_USE_THIS_BRANCH.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-echo 'If you cloned the main branch, you did it wrong. 
-Please read the readme and clone the correct branch.
-Installation has to be done manually. 
-Sorry for the inconvenience but I am too lazy'
+echo 'Use another branch, as said in the readme'
